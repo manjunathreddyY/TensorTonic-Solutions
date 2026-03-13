2f098799ab0597ae3686470520a69bcdc0dd3730
Manjunath Reddy <69053323+manjunathreddyY@users.noreply.github.com>
2026-03-13 12:43:17 +0100

    Update solution for binning

--- binning/binning.py
+++ binning/binning.py
@@ -4,14 +4,16 @@ def binning(values, num_bins):
     """
     # Write code here
     l = len(values)
+    min_value = min(values)
+    max_value = max(values)
     res = [0] * l
-    w = (max(values) - min(values)) / num_bins
+    w = (max_value - min_value) / num_bins
     if l == values.count(values[0]):
         return res
     else:
         for i,x in enumerate(values):
-            res[i] = (x - min(values)) // w
-            if x == max(values):
+            res[i] = (x - min_value) // w
+            if x == max_value:
                 res[i] = num_bins - 1
         return res
             
